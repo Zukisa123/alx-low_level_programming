ffd96ea885ddd09c6252c22729782e0eaa567efd
Zukisa <nomoyizukisa@yahoo.com>
2023-06-19 12:02:47 -0700

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char  a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeoF(a));
+printf("Size of a int: %1u byte(s)\n", (unsigned long)sizeoF(b));
+printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeoF(c));
+printf("Size of a long long int: %1u byte(s)\n", (unsigned long)sizeoF(d));
+printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeoF(f));
+return (0);
+}
